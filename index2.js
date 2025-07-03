b1ed1043926472f90efdb8f7fd6f2c0c3c374b51
Navya2065 <navyabhatia2065@gmail.com>
2025-07-03 15:33:38 +0530

    dated 3-07-2025

--- index2.js
+++ index2.js
@@ -9,12 +9,26 @@ setTimeout(()=>{
   console.log("settimeout");
 },0);
 fs.readFile("demo.txt","utf-8",(data)=>{
-  console.log(data);
+  console.log("data read");
   setTimeout(()=>{
     console.log("time2");
   },0)
   setImmediate(()=>{
     console.log("immediate 2");
   })
+})
+function someTask(){
+   return  new Promise((resolve,reject)=>{
+    resolve("promise");
+   })
+}
+someTask().then((data)=>{
+    console.log(data);
+})
+.catch((err)=>{
+    console.log(err);
+})
+process.nextTick(()=>{
+    console.log("next tick")
 })
 console.log("end");
